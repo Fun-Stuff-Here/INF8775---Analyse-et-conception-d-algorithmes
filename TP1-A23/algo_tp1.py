deadb7f8c58553fa75fc68d5aa109365e0fcc65e
Lopik45 <alexandre-1.turcotte@polymtl.ca>
2023-01-24 10:26:35 -0500

    algo conv fini

--- TP1-A23/algo_tp1.py
+++ TP1-A23/algo_tp1.py
@@ -1,18 +1,16 @@
 import numpy as np
 
 def conv(mat1, mat2, size):
-    mat1 = np.linalg.inv(mat1)
+    mat1 = np.transpose(mat1)
     matRes = np.empty([size,size])
-    lineRes = np.empty([size])
-    res = 0
     for iColMat1 in range(len(mat1)):
         for iLineMat2 in range(len(mat2)):
-            for index in range(len(mat1[iColMat1])):
+            res = 0
+            
+            for index in range(len(mat1[iColMat1])): 
                 res += mat1[iColMat1][index] * mat2[iLineMat2][index]
             
-            lineRes = np.insert(lineRes, iLineMat2, res)
-
-        matRes = np.insert(matRes, iColMat1, lineRes)
+            matRes[iColMat1, iLineMat2] = res
     
     return matRes
         
