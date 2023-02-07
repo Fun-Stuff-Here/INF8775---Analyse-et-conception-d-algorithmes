9044f8be49231064a9e006f07eac4e67ae77ef79
Nicolas Depelteau <nicolas@depelteau.ca>
2023-02-07 08:46:03 -0500

    test puissance

--- TP1-A23/analyse.py
+++ TP1-A23/analyse.py
@@ -2,17 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# df = pd.read_csv('results.csv').groupby(['algo','size']).mean().reset_index()
-# g = sns.FacetGrid(df, hue='algo', size=4, aspect=1)
-# g = g.map(plt.plot, 'size', 'temps')
-# g.set(xscale='log')
-# g.set(yscale='log')
-# g.add_legend()
-# plt.savefig('test_puissance')
-
-
 df = pd.read_csv('results.csv').groupby(['algo','size']).mean().reset_index()
-df['rapport'] = df['temps']/df['size']**3
-g2 = sns.lineplot(df, x="size", y="rapport", hue='algo', size=4, aspect=1)
-g2.add_legend()
-plt.savefig('test_rapport')
+df['size'] = 2**df['size']
+g = sns.FacetGrid(df, hue='algo',  aspect=1)
+g = g.map(plt.plot, 'size', 'temps')
+g.set(xscale='log')
+g.set(yscale='log')
+g.add_legend()
+plt.savefig('test_puissance')
+
+
+# df = pd.read_csv('results.csv').groupby(['algo','size']).mean().reset_index()
+# df['rapport'] = df['temps']/df['size']**3
+# g2 = sns.lineplot(df, x="size", y="rapport", hue='algo', size=4, aspect=1)
+# g2.add_legend()
+# plt.savefig('test_rapport')
