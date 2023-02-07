34fa85b8da780b15f3d3004ef607bf07e20b2ff7
Nicolas Depelteau <nicolas@depelteau.ca>
2023-02-07 10:42:43 -0500

    graph 2.0

--- TP1-A23/analyse.py
+++ TP1-A23/analyse.py
@@ -12,7 +12,6 @@ def plot_test_puissance():
     for algo in df['algo'].unique():
         slope, intercept, r_value, p_value, std_err = stats.linregress( np.log(df[df['algo']==algo]['size']),np.log(df[df['algo']==algo]['temps']))
         print(f"Temps = {slope} * Fn + {intercept} pour l'algorithme {algo}")
-
     g = g.map(plt.plot, 'size', 'temps')
     g.set(xscale='log')
     g.set(yscale='log')
@@ -23,24 +22,38 @@ def plot_test_puissance():
 def plot_test_rapport():
     df = pd.read_csv('results.csv').groupby(['algo','size']).mean().reset_index()
     df['size'] = 2**df['size']
-    df['rapport'] = df['temps']/df['size']**2.59
-    g2 = sns.lineplot(df, x="size", y="rapport", hue='algo')
     for algo in df['algo'].unique():
-        last_point = df[df['algo']==algo].tail(1)['rapport'].values[0]
-        print(f"Rapport = {last_point} pour l'algorithme {algo}")
-    plt.savefig('test_rapport')
-    plt.close()
+        data = df.filter(df[df['algo'] ==algo])
+        if algo == 'conv':
+            data['rapport'] = data['temps']/data['size']**3
+        elif algo == 'strassen':
+            data['rapport'] = data['temps']/data['size']**2.807
+        elif algo == 'strassenSeuil':
+            data['rapport'] = data['temps']/data['size']**2.75
+        else : 
+            raise ValueError(f"Algorithme {algo} inconnu")
+        sns.lineplot(data, x="size", y="rapport")
+        plt.savefig(f"test_rapport_{algo}")
+        plt.close()
 
 def plot_test_constante():
     df = pd.read_csv('results.csv').groupby(['algo','size']).mean().reset_index()
     df['size'] = 2**df['size']
-    df['Fn'] = df['size']**3
     for algo in df['algo'].unique():
-        slope, intercept, r_value, p_value, std_err = stats.linregress(df[df['algo']==algo]['Fn'],df[df['algo']==algo]['temps'])
+        data = df.filter(df[df['algo'] ==algo])
+        if algo == 'conv':
+            data['Fn'] = data['size']**3
+        elif algo == 'strassen':
+            data['Fn'] = data['size']**2.807
+        elif algo == 'strassenSeuil':
+            data['Fn'] = data['size']**2.75
+        else : 
+            raise ValueError(f"Algorithme {algo} inconnu")
+        slope, intercept, r_value, p_value, std_err = stats.linregress(data['Fn'],data['temps'])
         print(f"Temps = {slope} * Fn + {intercept} pour l'algorithme {algo}")
-    g2 = sns.lineplot(df, x="Fn", y="temps", hue='algo')
-    plt.savefig('test_constante')
-    plt.close()
+        sns.lineplot(data, x="Fn", y="temps")
+        plt.savefig(f"test_constante_{algo}")
+        plt.close()
 
 plot_test_puissance()
 plot_test_rapport()
