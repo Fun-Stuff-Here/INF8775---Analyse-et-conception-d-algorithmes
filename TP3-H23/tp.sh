5f71fdf0d6c9b5bad9b81115749586db21c5b395
Lopik45 <alexandre-1.turcotte@polymtl.ca>
2023-04-11 10:56:31 -0400

    test 1 de minizinc python

---
+++ TP3-H23/tp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pip3 install minizinc
+pip3 install minizinc[dzn]
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -e|--exemple)
+      PATH_TO_EX="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--print)
+    PRINT_FLAG='-p'
+    shift # past argument
+    ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+  esac
+done
+
+python3 test.py -e $PATH_TO_EX $PRINT_FLAG
